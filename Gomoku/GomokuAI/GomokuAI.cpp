efaec7fc0d763b65773f8aeb4651aa2b350998e3
Ice world <ice_world@outlook.com>
2020-12-27 21:27:05 +0800

    AI1.0

--- Gomoku/GomokuAI/GomokuAI.cpp
+++ Gomoku/GomokuAI/GomokuAI.cpp
@@ -5,6 +5,7 @@
 
 FILE* fp;
 FILE* in;
+FILE* test;
 void init();
 void input();
 void Judge(int x,int y);
@@ -22,6 +23,7 @@ int main()
 			Judge(i,j);
 	input();
 	fclose(in);
+	fclose(test);
 	return 0;
 }
 
@@ -34,22 +36,31 @@ void init()
 			fscanf(fp, "%d", &chessboard[i][j]);
 	}
 	fclose(fp);
+
 	memset(score, 0, sizeof(score));
 }
 void input()
 {
 	in = fopen("input.in", "w");
-	int x, y;
+	test = fopen("test.in", "w");
+	int x = 1, y = 1;
 	int max = 0;
 	for (int i = 1; i < SIZE; i++)
 	{
 		for (int j = 1; j < SIZE; j++)
-			if (score[i][j] > max)
+		{
+			if (score[i][j] >= max)
 			{
 				x = i;
 				y = j;
 				max = score[i][j];
 			}
+			if (chessboard[i][j] == 0)
+				fprintf(test, "%5d", score[i][j]);
+			else
+				fprintf(test, "    5");
+		}
+		fprintf(test, "\n");
 	}
 	fprintf(in, "%d %d", x, y);
 }
@@ -70,21 +81,26 @@ void Judge(int x,int y)
 		opp_score = num_count(x, y, i, -color)+ num_count(x, y, i + 4, -color);
 		f = my_score;
 		if (f >= 3)
-			score[x][y] += 4;
+			score[x][y] += f*1000;
+		else
 		f = opp_score;
 		if (f >= 3)
-			score[x][y] += 100000;
+			score[x][y] += f*1000;
 	}
-	score[x][y] += (my_score + opp_score);
+	score[x][y] += (my_score + opp_score) * 10;
 }
 int num_count(int x, int y, int d, int color)      //«¯”Ú…®√Ë
 {
 	int count = 0;
+	if (chessboard[x + dir[d][0]][y + dir[d][1]] == -color)
+		return 0;
 	for (int i = 1; i < 5; i++)
 	{
 		if (x + i * dir[d][0] >= SIZE || x + i * dir[d][0] < 1 || y + i * dir[d][1] >= SIZE || y + i * dir[d][1] < 1)
 			return count;
-		if (chessboard[x + i * dir[d][0]][y + i * dir[d][1]] != color)
+		if (chessboard[x + i * dir[d][0]][y + i * dir[d][1]] == -color)
+				return --count;
+		else if(chessboard[x + i * dir[d][0]][y + i * dir[d][1]] == 0)
 			return count;
 		count++;
 	}
